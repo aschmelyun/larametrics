6acb8ba275ad2947f3acd71347507075f1a144fd
Adi <skadimoolam@gmail.com>
2018-10-30 12:07:51 +0530

    Fixed pageTitle expression

--- src/resources/views/common/default.blade.php
+++ src/resources/views/common/default.blade.php
@@ -12,7 +12,7 @@
             <div class="my-3 my-md-5">
                 <div class="container">
                     <div class="page-header">
-                        <h1 class="page-title">{{ $pageTitle or 'Page Title Goes Here' }}</h1>
+                        <h1 class="page-title">{{ $pageTitle ?: 'Page Title Goes Here' }}</h1>
                         @if(isset($pageSubtitle))
                             <div class="text-muted mb-0 ml-4">{{ $pageSubtitle }}</div>
                         @endif
