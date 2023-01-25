1fda35c2dcbb08ffcf2e07eec959c49db92dd197
Andrew Schmelyun <schmelyun@gmail.com>
2023-01-24 23:34:37 -0500

    Adds dashboard view, blank css file

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en_US">
+
+<head>
+    <title>Larametrics Dashboard</title>
+</head>
+
+<body>
+    <h1>Dashboard!</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Larametrics dashboard!';
-});
+Route::view('/', 'larametrics::dashboard');
