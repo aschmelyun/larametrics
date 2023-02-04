530d386652611f2cbac4f5cf40fc49158e9f2911
Andrew Schmelyun <schmelyun@gmail.com>
2023-02-04 15:56:10 -0500

    Adds package-lock, runs pint

--- src/Larametrics.php
+++ src/Larametrics.php
@@ -8,16 +8,16 @@ class Larametrics
 {
     public function routes(array|string $middleware = []): void
     {
-        Route::middleware(['web', ...(array)$middleware])
+        Route::middleware(['web', ...(array) $middleware])
             ->prefix(config('larametrics.prefix'))
             ->name(config('larametrics.name_prefix'))
-            ->group(__DIR__ . '/../routes/web.php');
+            ->group(__DIR__.'/../routes/web.php');
 
         if (config('larametrics.enable_api')) {
-            Route::middleware(['api', ...(array)$middleware])
+            Route::middleware(['api', ...(array) $middleware])
                 ->prefix(config('larametrics.prefix'))
                 ->name(config('larametrics.name_prefix'))
-                ->group(__DIR__ . '/../routes/api.php');
+                ->group(__DIR__.'/../routes/api.php');
         }
     }
 }
