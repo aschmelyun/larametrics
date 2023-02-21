9850a7b8315aa1c35e28dac521bcf3c689f35cd3
Andrew Schmelyun <schmelyun@gmail.com>
2023-02-21 02:41:09 -0500

    Adds response code to request object return

--- src/Actions/FormatRequestObject.php
+++ src/Actions/FormatRequestObject.php
@@ -24,6 +24,7 @@ class FormatRequestObject implements Action
             'route' => $event['route'],
             'user' => $event['user'],
             'is_unique' => $event['is_unique'],
+            'response_code' => $event['response']->getStatusCode(),
         ];
 
         return $next($request);
