56862309e32d0ba0180d36fa5b4f6f23d5b862f2
Andrew Schmelyun <schmelyun@gmail.com>
2023-03-19 16:46:45 -0400

    Adds dropdown for basic date selection

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,8 +65,15 @@
         </svg>
     </div>
     <div class="max-w-screen-lg mx-auto py-8">
-        <header>
+        <header class="flex justify-between items-center">
             <h1 class="text-2xl font-semibold text-gray-900 mb-0">Overview</h1>
+            <select id="dates" class="rounded bg-white border border-gray-300 text-gray-500 text-sm font-semibold px-4 py-2">
+                <option value="0">Last 24 hours</option>
+                <option value="1">Last 7 days</option>
+                <option value="2">Last 30 days</option>
+                <option value="3">Last 90 days</option>
+                <option value="4">Last 365 days</option>
+            </select>
         </header>
         <main>
             <div class="rounded bg-white w-full px-8 py-4 shadow mt-4 flex justify-between">
